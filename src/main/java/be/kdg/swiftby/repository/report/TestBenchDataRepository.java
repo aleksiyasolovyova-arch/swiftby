82049fb2c6c36905dfbf5f5c97a9fbb248b7f6d1
Daniil0-0 <daniil.mumladze.dev@gmail.com>
2025-02-24 11:16:08 +0100

    Added jpa repo's for the report package

--- src/main/java/be/kdg/swiftby/repository/report/TestBenchDataRepository.java
+++ src/main/java/be/kdg/swiftby/repository/report/TestBenchDataRepository.java
@@ -1,4 +1,4 @@
-package be.kdg.swiftby.domain.repository.report;
+package be.kdg.swiftby.repository.report;
 
 import be.kdg.swiftby.domain.report.TestBenchData;
 import org.springframework.data.jpa.repository.JpaRepository;
