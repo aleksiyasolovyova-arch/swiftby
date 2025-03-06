3397e3398579f971d464a06b591d5493b5c3a9d8
alinadimova <alina.dimova@student.kdg.be>
2025-03-06 16:08:22 +0100

    .

--- build/resources/main/static/js/csv-results.js
+++
@@ -1,6 +0,0 @@
-const resultBtn = document.querySelector('#results')
-resultBtn.addEventListener('click', function () {
-    fetch('/api/csv/results')
-        .then(response => response.json()
-            .then(console.log(response)))
-});
