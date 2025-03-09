8a8aabf6241c043950d182ace2375b941dc2ae76
aleks <aleksiya.solovyova@student.kdg.be>
2025-03-09 16:53:03 +0100

    still needs debugging

--- src/main/java/be/kdg/swiftby/security/SecurityConfig.java
+++ src/main/java/be/kdg/swiftby/security/SecurityConfig.java
@@ -3,8 +3,11 @@ package be.kdg.swiftby.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
@@ -14,15 +17,26 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
+    SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity, GlobalAuthenticationConfigurerAdapter enableGlobalAuthenticationAutowiredConfigurer) throws Exception {
         return httpSecurity
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/", "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .requestMatchers(
                                 antMatcher("/js/**"),
                                 antMatcher("/webjars/**"),
-                                antMatcher("/css/**")).permitAll()
+                                antMatcher("/css/**"),
+                                antMatcher("/images/**")).permitAll()
                                 .anyRequest().authenticated())
+                .exceptionHandling(
+                        exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
+                            if (request.getRequestURI().startsWith("/api")) {
+                                response.setStatus(HttpStatus.FORBIDDEN.value());
+                            } else {
+                                response.sendRedirect("/login");
+                            }
+                        }))
+//                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(login -> login.loginPage("/login").permitAll())
                 .build();
     };
