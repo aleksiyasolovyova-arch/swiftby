494136a0424575ade0979200307c7600c0f2d03f
Daniil0-0 <daniil.mumladze.dev@gmail.com>
2025-05-03 22:40:28 +0200

    added a page for editing technicians' info or deleting them

--- src/main/java/be/kdg/swiftby/presentation/webapi/FacilityApiController.java
+++ src/main/java/be/kdg/swiftby/presentation/webapi/FacilityApiController.java
@@ -218,6 +218,12 @@ public class FacilityApiController {
                     administratorId, facilityId);
             return ResponseEntity.ok(admin);
     }
+    // TODO: MOVE TO A TECHNICIAN CONTROLLER SINCE IT'S NOT FACILITY SPECIFIC
+    @DeleteMapping("/technicians/{technicianId}")
+    public ResponseEntity<Void> deleteTechnician(@PathVariable Long technicianId) {
+        technicianService.remove(technicianId);
+        return ResponseEntity.noContent().build();
+    }
 
 
 }
