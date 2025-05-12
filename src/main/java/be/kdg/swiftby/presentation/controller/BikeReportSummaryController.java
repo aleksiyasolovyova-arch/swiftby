3fce0f6ed99cc25d4020c613e8112a8ededa4ac6
mj <mohamed-jan.jalloh@student.kdg.be>
2025-05-12 13:34:26 +0200

    Email Setup done and receival of emails is complete

--- src/main/java/be/kdg/swiftby/presentation/controller/BikeReportSummaryController.java
+++ src/main/java/be/kdg/swiftby/presentation/controller/BikeReportSummaryController.java
@@ -1,5 +1,12 @@
 package be.kdg.swiftby.presentation.controller;
 
+import be.kdg.swiftby.domain.report.BikeReportSummary;
+import be.kdg.swiftby.email.EmailService;
+import be.kdg.swiftby.service.intf.BikeReportSummaryPdfService;
+import be.kdg.swiftby.service.intf.BikeReportSummaryService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +14,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BikeReportSummaryController {
-    
+
+    private final EmailService emailService;
+    private final BikeReportSummaryPdfService bikeReportSummaryPdfService;
+    private final BikeReportSummaryService bikeReportSummaryService;
+
+    public BikeReportSummaryController(EmailService emailService, BikeReportSummaryPdfService bikeReportSummaryPdfService, BikeReportSummaryService bikeReportSummaryService) {
+        this.emailService = emailService;
+        this.bikeReportSummaryPdfService = bikeReportSummaryPdfService;
+        this.bikeReportSummaryService = bikeReportSummaryService;
+    }
+
     @GetMapping("/report-summary")
-    public String showSummaryPage(@RequestParam Long id, Model model) {
+    public String showSummaryPage(@RequestParam Long id ,Model model) {
+        BikeReportSummary summary = bikeReportSummaryService.getSummaryById(id);
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = null;
+
+        if (auth != null && auth.getPrincipal() instanceof UserDetails userDetails) {
+            userEmail = userDetails.getUsername();
+        }
+
+        if (summary != null && userEmail != null) {
+            byte[] pdf = bikeReportSummaryPdfService.generatePdf(summary);
+            try {
+                emailService.sendSummaryEmailWithAttachment(
+                        userEmail,
+                        "Bike Report Summary",
+                        "Your bike report summary is here and is attached!",
+                        pdf,
+                        "BikeReport_" + id + ".pdf"
+                );
+            } catch (Exception e) {
+                model.addAttribute("emailError", e.getMessage());
+            }
+        }
+
         model.addAttribute("summaryId", id);
         return "bike_report_summary";
     }
