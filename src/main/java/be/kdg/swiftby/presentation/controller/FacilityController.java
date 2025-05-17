548ec660a5b837ff710a0f09187ac535b71ae822
aleks <aleksiya.solovyova@student.kdg.be>
2025-05-17 22:47:15 +0200

    little forbidden page thingy for the facilities

--- src/main/java/be/kdg/swiftby/presentation/controller/FacilityController.java
+++ src/main/java/be/kdg/swiftby/presentation/controller/FacilityController.java
@@ -1,12 +1,11 @@
 package be.kdg.swiftby.presentation.controller;
 
 import be.kdg.swiftby.security.CustomUserDetails;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.server.ResponseStatusException;
+
 
 @Controller
 public class FacilityController {
--- src/main/java/be/kdg/swiftby/security/SecurityConfig.java
+++ src/main/java/be/kdg/swiftby/security/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,7 +16,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
