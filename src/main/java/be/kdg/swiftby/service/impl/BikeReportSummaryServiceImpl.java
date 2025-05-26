2c9fec3e099a35c304eeea956119b421dedf74c2
alinadimova <alina.dimova@student.kdg.be>
2025-05-26 12:25:40 +0200

    fixing commit

--- src/main/java/be/kdg/swiftby/service/impl/BikeReportSummaryServiceImpl.java
+++ src/main/java/be/kdg/swiftby/service/impl/BikeReportSummaryServiceImpl.java
@@ -410,7 +410,7 @@ public class BikeReportSummaryServiceImpl implements BikeReportSummaryService {
     }
 
 
-
+//meow
     @Override
     public List<ServiceSummaryIdDateDto> getAvailableComparisons(Long summaryId) {
         BikeReportSummary current = getSummaryById(summaryId);
