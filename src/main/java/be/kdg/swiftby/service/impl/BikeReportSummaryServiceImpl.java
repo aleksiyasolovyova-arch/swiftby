4d8e796480586894900d8ed16c323e9b3782e8f9
alinadimova <alina.dimova@student.kdg.be>
2025-05-26 12:51:00 +0200

    meow

--- src/main/java/be/kdg/swiftby/service/impl/BikeReportSummaryServiceImpl.java
+++ src/main/java/be/kdg/swiftby/service/impl/BikeReportSummaryServiceImpl.java
@@ -410,7 +410,7 @@ public class BikeReportSummaryServiceImpl implements BikeReportSummaryService {
     }
 
 
-//meow
+//meoww
     @Override
     public List<ServiceSummaryIdDateDto> getAvailableComparisons(Long summaryId) {
         BikeReportSummary current = getSummaryById(summaryId);
